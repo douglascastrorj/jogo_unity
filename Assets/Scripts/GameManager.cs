61c088f6024e996222fde6650986fe7d0ab77db5
Douglas Castro da Silva <douglascastrorj@gmail.com>
2019-02-09 19:50:44 -0200

    jogo com primeira parte do cenario e 2 pulos funcionando

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+using Image = UnityEngine.UI.Image;
+
+
+public class GameManager : MonoBehaviour {
+
+    public bool isPaused = false;
+
+    public GameObject painelCompleto;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Resume()
+    {
+        painelCompleto.SetActive(false);
+    }
+
+    public void Pause()
+    {
+        painelCompleto.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerBehaviour : MonoBehaviour {
+
+    public int lifes;
+    public int rings;
+    public float velocidade;
+    public float forcaPulo;
+    public string nome;
+
+    public Text textLifes;
+    public Text textRings;
+
+    private Rigidbody2D rigidBody;
+    private Animator animator;
+
+    private bool isOnGround = true;
+    private int maxJumps = 2;
+    private int countJumps = 0;
+
+    // Use this for initialization
+    void Start () {
+        Rigidbody2D rigidBody = GetComponent<Rigidbody2D>();
+        Animator animator = GetComponent<Animator>();
+
+        this.rigidBody = rigidBody;
+        this.animator = animator;
+
+        textLifes.text = lifes.ToString();
+        textRings.text = rings.ToString();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.gameObject.tag);
+        if (collision.gameObject.CompareTag("plataforma"))
+        {
+            isOnGround = true;
+            countJumps = 0;
+            animator.SetBool("jumping", false);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("plataforma"))
+        {
+            isOnGround = false;
+        }
+    }
+
+    void jump()
+    {
+        float movimento = Input.GetAxis("Horizontal");
+        rigidBody.velocity = new Vector2(movimento * velocidade, rigidBody.velocity.y);
+        if (Input.GetKeyDown(KeyCode.Space) && (isOnGround || countJumps < maxJumps))
+        {
+            rigidBody.velocity = new Vector2(0, forcaPulo);
+            //rigidBody.AddForce(new Vector2(0, forcaPulo));
+            countJumps++;
+            animator.SetBool("jumping", true);
+            
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        float movimento = Input.GetAxis("Horizontal");
+        
+
+        bool isWalking = movimento != 0;
+        animator.SetBool("walking", isWalking);
+        if(movimento < 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+        else if(movimento > 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+
+        this.jump();
+
+    }
+}
